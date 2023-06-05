89b20ab027b1bf5b1708ecea3e91e8fbefc9c460
Lauren Johnston <hilotraining@outlook.com>
2023-06-05 13:40:27 -0600

    began adding login button to react 16. Decided to switch to react 18 and refactor all the code so far. Will do that in this next commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Nav from "./components/Nav";
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+  render() {
+    return (
+      <>
+        <Nav auth={this.auth} />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
